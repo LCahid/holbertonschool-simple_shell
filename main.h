37d46b86a4b220c06d316e296debdda175b729ae
root <gqasimova93@gmail.com>
2023-12-29 05:48:27 -0800

    file find added and some memory leaks are fixed beside that path_variable free function added for free memory

--- main.h
+++ main.h
@@ -1,6 +1,6 @@
 #ifndef MAINH
 #define MAINH
-
+#define PATH "PATH"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,8 +8,15 @@
 #include <sys/wait.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <errno.h>
 
+char **path_var;
 extern char **environ;
 char **get_command(char **);
+char **get_path(char **);
+char *find_file(char *command, int *);
+void print_env(void);
+void print_prompt(int status);
+void free_path(void);
 
 #endif
--- test/shell.c
+++ test/shell.c
@@ -1,26 +1,21 @@
 #include "main.h"
-#include <errno.h>
 /**
   * main - shell start function
   * @argv: argument variables
-  * @argc: argument counter
+  * @argc: argument count
+  * @env: env variables
   * Return: always 0
   */
-int main(int argc, char **argv)
+int main(int argc, char **argv, char **env)
 {
 	char **fcommand, *buf;
 	pid_t pid;
-	struct stat st;
-	int status;
+	int status, lk = 0;
 
-	(void)argc;
+	(void)argc, path_var = get_path(env);
 	while (1)
 	{
-		status = isatty(STDIN_FILENO);
-		if (status)
-			printf("#cisfun$ ");
-		else
-			errno = 0;
+		status = isatty(STDIN_FILENO), print_prompt(status);
 		fcommand = get_command(&buf);
 		if (fcommand == NULL)
 		{
@@ -29,12 +24,15 @@ int main(int argc, char **argv)
 				printf("\n");
 			break;
 		}
-		if (stat(fcommand[0], &st) == -1)
+		if (!strcmp("env", fcommand[0]))
 		{
-			if (!strcmp("env", fcommand[0]))
-				print_env();
-			else
-				perror(argv[0]), free(buf), free(fcommand);
+			print_env(), free(buf), free(fcommand);
+			continue;
+		}
+		fcommand[0] = find_file(fcommand[0], &lk);
+		if (!fcommand[0])
+		{
+			perror(argv[0]), free(buf), free(fcommand), errno = 0;
 			continue;
 		}
 		pid = fork();
@@ -42,7 +40,11 @@ int main(int argc, char **argv)
 			execve(fcommand[0], fcommand, environ);
 		else
 			wait(NULL);
-		free(buf), free(fcommand);
+		free(buf);
+		if (lk)
+			free(fcommand[0]), lk = 0;
+		free(fcommand);
 	}
+	free_path();
 	return (0);
 }
